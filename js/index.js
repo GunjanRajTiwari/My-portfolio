2cb38e1ff196a8610c7c9249ad19423ab753f399
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-06-28 18:54:27 +0545

    finishing and theme select

--- js/index.js
+++ js/index.js
@@ -4,3 +4,7 @@ const body = document.body;
 theme.addEventListener("click", e => {
 	body.classList.toggle("dark-theme");
 });
+
+function changeColor(hue) {
+	document.documentElement.style.setProperty("--primary", hue);
+}
