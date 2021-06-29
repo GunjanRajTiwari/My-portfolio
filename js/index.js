88de2d5df196de44f90f04364b7d512ff5e40e14
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-06-29 11:41:51 +0545

    responsive navbar made

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const theme = document.getElementById("theme");
+const hamburger = document.getElementById("hamburger");
+const navbar = document.getElementById("navbar");
 const body = document.body;
 
 theme.addEventListener("click", e => {
@@ -13,3 +15,14 @@ theme.addEventListener("click", e => {
 function changeColor(hue) {
 	document.documentElement.style.setProperty("--primary", hue);
 }
+
+navbar.onclick = removeSlide;
+body.onscroll = removeSlide;
+
+function removeSlide() {
+	navbar.classList.remove("slide");
+}
+
+hamburger.addEventListener("click", e => {
+	navbar.classList.toggle("slide");
+});
