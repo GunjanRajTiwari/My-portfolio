128df2a8eacda90eec29706faa2fd8a3bd1f9f62
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-07-01 12:57:33 +0545

    moved counter to js

--- js/index.js
+++ js/index.js
@@ -26,3 +26,9 @@ function removeSlide() {
 hamburger.addEventListener("click", e => {
 	navbar.classList.toggle("slide");
 });
+
+const counter = document.getElementById("counter");
+counter.setAttribute(
+	"src",
+	"https://hitwebcounter.com/counter/counter.php?page=7832010&style=0006&nbdigits=5&type=page&initCount=103"
+);
